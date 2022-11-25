73c94bc21f5b6da5bf972ca88c31214cc14dd4ee
markusremplbauer <markus.remplbauer@gmx.at>
2022-11-25 12:02:39 +0100

    update MainActivity

--- frontend/ShoppingList/app/src/main/java/at/htl/shoppinglist/MainActivity.kt
+++ frontend/ShoppingList/app/src/main/java/at/htl/shoppinglist/MainActivity.kt
@@ -10,34 +10,24 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import at.htl.shoppinglist.navigation.SetupNavigation
 import at.htl.shoppinglist.ui.theme.ShoppingListTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ShoppingListTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                navController = rememberNavController()
+                SetupNavigation(navController = navController)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ShoppingListTheme {
-        Greeting("Android")
     }
 }
