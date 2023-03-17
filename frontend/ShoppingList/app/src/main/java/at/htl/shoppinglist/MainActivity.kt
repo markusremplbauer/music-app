6943f4ffbf208bed8f45f0fbb78f60fccfe05237
markusremplbauer <markus.remplbauer@gmx.at>
2023-03-17 11:41:41 +0100

    add tabs to MainActivity

--- frontend/ShoppingList/app/src/main/java/at/htl/shoppinglist/MainActivity.kt
+++ frontend/ShoppingList/app/src/main/java/at/htl/shoppinglist/MainActivity.kt
@@ -4,31 +4,55 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
 import androidx.navigation.NavHostController
 import at.htl.shoppinglist.ui.screens.list.DrinkList
 import at.htl.shoppinglist.ui.viewmodels.DrinkViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
+import androidx.compose.material.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.*
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    private lateinit var navController: NavHostController
     private val drinkViewModel by viewModels<DrinkViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            TODO update when continuing with the tutorial
-//            ShoppingListTheme {
-//                navController = rememberNavController()
-//                SetupNavigation(navController = navController)
-//            }
-            Surface(color = MaterialTheme.colors.background) {
-                DrinkList(drinkList = drinkViewModel.drinkListResponse)
-                drinkViewModel.getDrinkList()
+            Tabs(drinkViewModel)
+        }
+    }
+}
+
+
+@Composable
+fun DrinksTab(drinkViewModel: DrinkViewModel) {
+    Surface(color = MaterialTheme.colors.background) {
+        DrinkList(drinkList = drinkViewModel.drinkListResponse)
+        drinkViewModel.getDrinkList()
+    }
+}
+
+
+@Composable
+fun Tabs(drinkViewModel: DrinkViewModel) {
+    var tabIndex by remember { mutableStateOf(0) }
+
+    val tabTitles = listOf("Drinks", "Test")
+    Column {
+        TabRow(selectedTabIndex = tabIndex) {
+            tabTitles.forEachIndexed { index, title ->
+                Tab(selected = tabIndex == index,
+                    onClick = { tabIndex = index },
+                    text = { Text(text = title) })
             }
+        }
+        when (tabIndex) {
+            0 -> DrinksTab(drinkViewModel = drinkViewModel)
         }
     }
 }
