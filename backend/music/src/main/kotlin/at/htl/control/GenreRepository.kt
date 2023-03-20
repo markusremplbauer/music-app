cdb0db743cf76a0976d5c721acb762efa7983a7e
markusremplbauer <markus.remplbauer@gmx.at>
2023-03-20 18:40:48 +0100

    add repositories

--- backend/music/src/main/kotlin/at/htl/control/GenreRepository.kt
+++ backend/music/src/main/kotlin/at/htl/control/GenreRepository.kt
@@ -1,9 +1,9 @@
 package at.htl.control
 
-import at.htl.entity.Artist
+import at.htl.entity.Genre
 import io.quarkus.hibernate.orm.panache.kotlin.PanacheRepository
 import javax.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
-class ArtistRepository: PanacheRepository<Artist> {
+class GenreRepository: PanacheRepository<Genre> {
 }
