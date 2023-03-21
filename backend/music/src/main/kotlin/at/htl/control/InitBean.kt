456e3e1f24bd3fa37eaf6ddd5cb18c4488d8da1c
markusremplbauer <markus.remplbauer@gmx.at>
2023-03-21 14:09:35 +0100

    improve InitBean

--- backend/music/src/main/kotlin/at/htl/control/InitBean.kt
+++ backend/music/src/main/kotlin/at/htl/control/InitBean.kt
@@ -4,6 +4,7 @@ import at.htl.entity.Artist
 import at.htl.entity.Genre
 import at.htl.entity.Song
 import io.quarkus.runtime.StartupEvent
+import org.jboss.logging.Logger
 import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -22,6 +23,9 @@ class InitBean {
     @Inject
     lateinit var genreRepository: GenreRepository
 
+    @Inject
+    lateinit var LOG: Logger
+
     fun init(@Observes event: StartupEvent) {
         readArtistsFromCsv()
         readGenresFromCsv()
@@ -31,79 +35,54 @@ class InitBean {
     @Transactional
     fun readArtistsFromCsv() {
         try {
-            val lines = Files.lines(Paths.get("artist.csv"))
-            lines.skip(1)
-                .forEach { l ->
+            Files.lines(Paths.get("artist.csv")).use { lines ->
+                lines.skip(1).forEach { l ->
                     val line = l.split(";")
                     val artist = Artist(line[0], line[1], line[2], line[3])
                     artistRepository.persist(artist)
                 }
+            }
         } catch (e: IOException) {
-            e.printStackTrace()
+            LOG.error("Failed to read artists from CSV file", e)
         }
     }
 
     @Transactional
     fun readGenresFromCsv() {
         try {
-            val lines = Files.lines(Paths.get("genres.csv"))
-            lines.skip(1)
-                .forEach { l ->
+            Files.lines(Paths.get("genres.csv")).use { lines ->
+                lines.skip(1).forEach { l ->
                     val line = l.split(";")
                     val genre = Genre(
-                        line[0],
-                        line[1]
+                        line[0], line[1]
                     )
                     genreRepository.persist(genre)
                 }
+            }
         } catch (e: IOException) {
-            e.printStackTrace()
+            LOG.error("Failed to read genres from CSV file", e)
         }
     }
 
     @Transactional
     fun readSongsFromCsv() {
         try {
-            val lines = Files.lines(Paths.get("songs.csv"))
-            lines.skip(1)
-                .forEach { l ->
-                    val line = l.split(";")
-                    val song = Song(
-                        line[0],
-                        artistRepository.findByAlias(line[1]),
-                        line[2].split(", ").map { genreRepository.findByName(it) },
-                    )
-                    songRepository.persist(song)
+            Files.lines(Paths.get("songs.csv")).use { lines ->
+                {
+                    lines.skip(1).forEach { l ->
+                        val line = l.split(";")
+                        val song = Song(
+                            line[0],
+                            artistRepository.findByAlias(line[1]),
+                            line[2].split(", ").map { genreRepository.findByName(it) },
+                        )
+                        songRepository.persist(song)
+                    }
                 }
+            }
+
         } catch (e: IOException) {
-            e.printStackTrace()
+            LOG.error("Failed to read songs from CSV file", e)
         }
     }
-
-
-//    @Transactional
-//    fun readGenresFromCsv(){
-//        try {
-//            val lines = Files.lines(Paths.get("genres.csv"))
-//            val genres = mutableSetOf<String>()
-//
-//            lines.skip(1)
-//                .forEach { l ->
-//                    for (genreName in l.split(",")) {
-//                        genres.add(genreName)
-//                    }
-//                }
-//
-//            for (genreName in genres) {
-//                val persistedGenre = genreRepository.findByName(genreName)
-//                if (persistedGenre == null) {
-//                    val genre = Genre(genreName, "Description for $genreName")
-//                    genreRepository.persist(genre)
-//                }
-//            }
-//        } catch (e: IOException) {
-//            e.printStackTrace()
-//        }
-//    }
-
 }
