61397eaed14296b5a533c0ad4bfe3fc188c9da24
markusremplbauer <markus.remplbauer@gmx.at>
2023-03-29 13:05:14 +0200

    add chips to songs for genres

--- frontend/music/app/src/main/java/at/htl/remplbauer/music/ui/songs/SongListScreen.kt
+++ frontend/music/app/src/main/java/at/htl/remplbauer/music/ui/songs/SongListScreen.kt
@@ -1,8 +1,7 @@
 package at.htl.remplbauer.music.ui.songs
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,6 +10,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Cyan
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
@@ -36,6 +37,23 @@ fun SongListScreen(viewModel: SongsViewModel) {
                         style = MaterialTheme.typography.h5,
                         fontWeight = FontWeight.Bold
                     )
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        song.genres.forEach {
+                            Text(
+                                text = it.name,
+                                style = MaterialTheme.typography.caption,
+                                modifier = Modifier
+                                    .background(
+                                        Color(Cyan.value),
+                                        RoundedCornerShape(8.dp)
+                                    )
+                                    .padding(horizontal = 8.dp, vertical = 4.dp)
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                        }
+                    }
                     Text(
                         text = song.artist.alias,
                         style = MaterialTheme.typography.body1
