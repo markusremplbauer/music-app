966242b20037a12a57e225a1282e79ce5b25d169
markusremplbauer <markus.remplbauer@gmx.at>
2023-04-14 10:55:36 +0200

    do not use * in queries

--- frontend/music/app/src/main/java/at/htl/remplbauer/music/model/db/dao/ArtistDao.kt
+++ frontend/music/app/src/main/java/at/htl/remplbauer/music/model/db/dao/ArtistDao.kt
@@ -8,13 +8,13 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface ArtistDao {
 
-    @Query("select * from artists")
+    @Query("select alias, firstName, lastName from artists")
     fun getAll(): LiveData<List<Artist>>
 
-    @Query("select * from artists where id = :authorId")
+    @Query("select alias, firstName, lastName from artists where id = :authorId")
     fun getById(authorId: Int): Flow<Artist>
 
-    @Query("select * from artists where alias LIKE :alias LIMIT 1")
+    @Query("select alias, firstName, lastName from artists where alias LIKE :alias LIMIT 1")
     fun findByAlias(alias: String): Artist?
 
 
